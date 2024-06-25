c11625e9441e021024ec50772a6096e846e8de3b
Farah <farahmemmi4@gmail.com>
2024-06-25 12:20:04 +0100

    angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //<app-root></app-root>
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainNavComponent,
+    // autres composants
+  ],
+  // autres configurations
+})
+export class AppModule { }
